723b5ba15914b7caebf414b71743f430659f7c60
rkaczynski <rafal.kaczynski@sabstt.com>
2021-01-18 11:17:51 +0000

    Edit README.md

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const descriptionText: string =
-	"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots  in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia";
+	"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots  in a piece of classical Latin literature from 45 BC, making it over 2000 years old...";
